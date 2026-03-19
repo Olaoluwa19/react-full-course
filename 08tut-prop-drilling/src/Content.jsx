71b8e8bfe981efe0dc0226a80208d330d66d31e9
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-19 05:16:21 +0100

    made cleanup in content component and drilled in the properties from app.jsx

--- 08tut-prop-drilling/src/Content.jsx
+++ 08tut-prop-drilling/src/Content.jsx
@@ -1,39 +1,6 @@
-import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsweetened",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "3 500g bags of Haldiram's Bhel Puri",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item,
-    );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (

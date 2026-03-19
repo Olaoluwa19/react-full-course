37226839919b0ea744bf5e01b56568a74ba2894f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-19 05:49:55 +0100

    feat: went further down the rabbit hole by drilling function through abstration to parent elements

--- 08tut-prop-drilling/src/Content.jsx
+++ 08tut-prop-drilling/src/Content.jsx
@@ -1,33 +1,14 @@
-import { FaTrashAlt } from "react-icons/fa";
+import ItemList from "./ItemList";
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                id={item.id}
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-                style={item.checked ? { textDecoration: "line-through" } : null}
-                htmlFor={item.id}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <FaTrashAlt
-                onClick={() => handleDelete(item.id)}
-                role="button"
-                tabIndex="0"
-              />
-            </li>
-          ))}
-        </ul>
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
       )}

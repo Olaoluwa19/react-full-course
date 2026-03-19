9509de46b2e90af74c773dc61c2b4aff8d6a078f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-19 05:53:22 +0100

    feat: started lesson 9

---
+++ 09tut-controlled-input/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsweetened",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "3 Bags of rice",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "3 500g bags of Haldiram's Bhel Puri",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  return (
+    <div className="App">
+      <Header title="Grocery List" />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
+    </div>
+  );
+}
+
+export default App;

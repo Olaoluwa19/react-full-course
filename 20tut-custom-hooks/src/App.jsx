847c533d4d97fcdd824595090651689336288214
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-28 17:46:35 +0100

    feat: added useAxiosFetch to App to interact with db

--- 20tut-custom-hooks/src/App.jsx
+++ 20tut-custom-hooks/src/App.jsx
@@ -10,6 +10,7 @@ import { useState, useEffect } from "react";
 import { format } from "date-fns";
 import api from "./api/posts";
 import useWindowSize from "./hooks/useWindowSize";
+import useAxiosFetch from "./hooks/useAxiosFetch";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -22,25 +23,13 @@ function App() {
   const navigate = useNavigate();
   const { width } = useWindowSize();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get("/posts");
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          //Not in the 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    };
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts",
+  );
 
-    fetchPosts();
-  }, []);
+  useEffect(() => {
+    setPosts(data);
+  }, [data]);
 
   useEffect(() => {
     const filteredResults = posts.filter(
@@ -98,7 +87,15 @@ function App() {
     <Routes>
       <Route
         path="/"
-        element={<Layout search={search} setSearch={setSearch} width={width} />}
+        element={
+          <Layout
+            search={search}
+            setSearch={setSearch}
+            width={width}
+            fetchError={fetchError}
+            isLoading={isLoading}
+          />
+        }
       >
         <Route index element={<Home posts={searchResults} />} />
         <Route path="post">

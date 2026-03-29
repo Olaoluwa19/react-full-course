6de604ce04b8d8f194c3031a47789f21ba053d73
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-29 19:02:26 +0100

    feat: cert up boiler plate for context api

---
+++ 21tut-context-api/src/NewPost.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          id="postTitle"
+          name="postTitle"
+          type="text"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          name="postBody"
+          id="postBody"
+          placeholder="What's on your mind?"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
+export default NewPost;

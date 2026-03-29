eb5ffc56b1e3317f6981be6a5e23b2f6a3fb9cb3
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-29 21:30:52 +0100

    feat: refactored with DataContext

--- 21tut-context-api/src/EditPost.jsx
+++ 21tut-context-api/src/EditPost.jsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+import api from "./api/posts";
 import DataContext from "./context/DataContext";
 
 const EditPost = () => {
-  const { posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle } =
-    useContext(DataContext);
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
+  const navigate = useNavigate();
+  const { posts, setPosts } = useContext(DataContext);
 
   const { id } = useParams();
   const post = posts.find((post) => post.id.toString() === id);
@@ -16,6 +19,22 @@ const EditPost = () => {
       setEditBody(post.body);
     }
   }, [post, setEditTitle, setEditBody]);
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post)),
+      );
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
   return (
     <main className="NewPost">
       {editTitle && (

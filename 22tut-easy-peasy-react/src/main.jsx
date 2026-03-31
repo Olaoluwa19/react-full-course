5c758ebfe8d7d7cf4c8f560c546d1625d55b45cc
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-31 07:45:29 +0100

    feat: refactored with easy-peasy,

--- 22tut-easy-peasy-react/src/main.jsx
+++ 22tut-easy-peasy-react/src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { StoreProvider } from "easy-peasy";
+import store from "./store";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
   </StrictMode>,
 );
